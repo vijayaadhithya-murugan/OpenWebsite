c1d6919981f1f1ca0e87b2a9b0ff95d81f8a0854
Vijay Aadhithya <m.vijayaadhithya@gmail.com>
2024-04-19 18:01:44 +0530

    LinkedIn Updated

--- src/data/allDetails.js
+++ src/data/allDetails.js
@@ -4,7 +4,7 @@ export const contactDetails ={
     "pincode": "626-123",
     "mobile": "+91-9487476052",
     "gmail": "m.vijayaadhithya@gmail.com",
-    "linkedIn":"www.linkedin.com/in/vijay-aadhithya",
+    "linkedIn":"https://linkedin.com/in/vijay-aadhithya",
     "github": "https://github.com/vijayaadhithya-murugan",
     "twitter": "https://twitter.com/aadhithya_vijay"
 };
